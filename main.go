36ed0c45aa0746e99f63456c337bbd96e11270a5
Anil Gupta <anilgupta83@gmail.com>
2026-05-28 09:54:58 +0530

    ...

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello, World!, service started")
-	select {}
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("Hello world"))
+	})
+	http.ListenAndServe(":8080", nil)
 }
